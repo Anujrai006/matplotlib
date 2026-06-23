553432fe06074a89db9aa5a39b3742aef4d07ea6
Anuj Rai <anujxilling253745@gmail.com>
2026-06-23 07:37:12 +0545

    introduction to matplotlib

---
+++ intro.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array([2023,2024,2025,2026]) #x cordinates in a lst
+y=np.array([15,20,25,30])
+plt.plot(x,y)
+plt.show()
+a=np.array([2,3,4,5,6])
+b=np.array([4,9,16,25,36])
+plt.plot(a,b)
+plt.show()
